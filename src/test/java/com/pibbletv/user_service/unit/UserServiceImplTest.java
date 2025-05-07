3d677381e778d73e5f95f144a8a70439a9efd631
TanzerDx <hristoganchev3@gmail.com>
2025-05-07 02:53:05 +0200

    unit tests created

--- src/test/java/com/pibbletv/user_service/unit/UserServiceImplTest.java
+++ src/test/java/com/pibbletv/user_service/unit/UserServiceImplTest.java
@@ -50,6 +50,67 @@ class UserServiceImplTest {
         ));
     }
 
+    @Test
+    void testUpdateUser_ExistingUser_UpdatesSuccessfully() {
+
+        UserEntity toUpdate = new UserEntity();
+        toUpdate.setId(123L);
+        toUpdate.setUsername("testuser");
+        toUpdate.setBgImage(imageLoader.load("images/default-bg.webp"));
+        toUpdate.setProfileImage(imageLoader.load("images/default-pfp.webp"));
+        toUpdate.setIsBanned(false);
+
+        User toPass = UserConverter.convertToObject(toUpdate);
+
+        when(userRepository.existsById(123L)).thenReturn(Mono.just(true));
+        when(userRepository.save(any(UserEntity.class))).thenReturn(Mono.just(new UserEntity()));
+
+        StepVerifier.create(userService.updateUser(toPass))
+                .verifyComplete();
+
+        verify(userRepository).save(argThat(user -> user.getUsername().equals("testuser")));
+    }
+
+    @Test
+    void banUser_ExistingUser_BanSuccessfully() {
+        UserEntity entity = new UserEntity();
+        entity.setId(123L);
+        entity.setUsername("testuser");
+        entity.setBgImage(imageLoader.load("images/default-bg.webp"));
+        entity.setProfileImage(imageLoader.load("images/default-pfp.webp"));
+        entity.setIsBanned(false);
+
+        when(userRepository.findByUsername("testuser")).thenReturn(Mono.just(entity));
+        when(userRepository.save(any(UserEntity.class))).thenReturn(Mono.just(entity));
+
+        StepVerifier.create(userService.banUser("testuser"))
+                .verifyComplete();
+
+        verify(userRepository).save(argThat(user ->
+                user.getUsername().equals("testuser") && Boolean.TRUE.equals(user.getIsBanned())
+        ));
+    }
+
+    @Test
+    void ubbanUser_ExistingUser_UnbanSuccessfully() {
+        UserEntity entity = new UserEntity();
+        entity.setId(123L);
+        entity.setUsername("testuser");
+        entity.setBgImage(imageLoader.load("images/default-bg.webp"));
+        entity.setProfileImage(imageLoader.load("images/default-pfp.webp"));
+        entity.setIsBanned(true);
+
+        when(userRepository.findByUsername("testuser")).thenReturn(Mono.just(entity));
+        when(userRepository.save(any(UserEntity.class))).thenReturn(Mono.just(entity));
+
+        StepVerifier.create(userService.unbanUser("testuser"))
+                .verifyComplete();
+
+        verify(userRepository).save(argThat(user ->
+                user.getUsername().equals("testuser") && Boolean.FALSE.equals(user.getIsBanned())
+        ));
+    }
+
 
     @Test
     void testGetUser_ExistingUser_ReturnsUser() {
@@ -93,6 +154,70 @@ class UserServiceImplTest {
 
         verify(userRepository, never()).save(any());
     }
+
+    @Test
+    void testUpdateUser_NotFound_DoesNotExist() {
+
+        UserEntity toUpdate = new UserEntity();
+        toUpdate.setId(123L);
+        toUpdate.setUsername("testuser");
+        toUpdate.setBgImage(imageLoader.load("images/default-bg.webp"));
+        toUpdate.setProfileImage(imageLoader.load("images/default-pfp.webp"));
+        toUpdate.setIsBanned(false);
+
+        User toPass = UserConverter.convertToObject(toUpdate);
+
+        when(userRepository.existsById(123L)).thenReturn(Mono.just(false));
+
+        StepVerifier.create(userService.updateUser(toPass))
+                .expectErrorMatches(throwable ->
+                        throwable instanceof IllegalArgumentException &&
+                                throwable.getMessage().equals("User does not exist"))
+                .verify();
+
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void banUser_ExistingUser_UserAlreadyBanned() {
+        UserEntity entity = new UserEntity();
+        entity.setId(123L);
+        entity.setUsername("testuser");
+        entity.setBgImage(imageLoader.load("images/default-bg.webp"));
+        entity.setProfileImage(imageLoader.load("images/default-pfp.webp"));
+        entity.setIsBanned(true);
+
+        when(userRepository.findByUsername("testuser")).thenReturn(Mono.just(entity));
+
+        StepVerifier.create(userService.banUser("testuser"))
+                .expectErrorMatches(throwable ->
+                        throwable instanceof IllegalStateException &&
+                                throwable.getMessage().equals("User is already banned"))
+                .verify();
+
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void ubbanUser_ExistingUser_UserIsNotBanned() {
+        UserEntity entity = new UserEntity();
+        entity.setId(123L);
+        entity.setUsername("testuser");
+        entity.setBgImage(imageLoader.load("images/default-bg.webp"));
+        entity.setProfileImage(imageLoader.load("images/default-pfp.webp"));
+        entity.setIsBanned(false);
+
+        when(userRepository.findByUsername("testuser")).thenReturn(Mono.just(entity));
+
+        StepVerifier.create(userService.unbanUser("testuser"))
+                .expectErrorMatches(throwable ->
+                        throwable instanceof IllegalStateException &&
+                                throwable.getMessage().equals("User is not banned"))
+                .verify();
+
+        verify(userRepository, never()).save(any());
+    }
+
 
     @Test
     void testGetUser_NotFound_ThrowsError() {
